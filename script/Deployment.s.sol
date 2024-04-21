b11ea34158d3b61fb2e5b5b880be837a69f8d0de
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-04-21 09:54:44 +0000

    chore(script): add link ERC20

--- script/Deployment.s.sol
+++ script/Deployment.s.sol
@@ -13,20 +13,24 @@ contract DeploymentScript is Script {
         ERC20WithMinters usdc = _deployToken({name: "USDC", symbol: "USDC", decimals: 6});
         ERC20WithMinters weth = _deployToken({name: "WETH", symbol: "WETH", decimals: 18});
         ERC20WithMinters wbtc = _deployToken({name: "WBTC", symbol: "WBTC", decimals: 8});
+        ERC20WithMinters link = _deployToken({name: "LINK", symbol: "LINK", decimals: 18});
 
         console.log("Mocked USDC deployed at", address(usdc));
         console.log("Mocked WETH deployed at", address(weth));
         console.log("Mocked WBTC deployed at", address(wbtc));
+        console.log("Mocked LINK deployed at", address(link));
 
         // 2. Deploy the mocked AMM.
-        address[] memory underlyings = new address[](2);
+        address[] memory underlyings = new address[](3);
         underlyings[0] = address(weth);
         underlyings[1] = address(wbtc);
+        underlyings[2] = address(link);
 
         // See here for price feed addresses: https://docs.chain.link/data-feeds/price-feeds/addresses?network=base&page=1#base-sepolia-testnet
-        address[] memory priceFeeds = new address[](2);
+        address[] memory priceFeeds = new address[](3);
         priceFeeds[0] = address(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
         priceFeeds[1] = address(0x0FB99723Aee6f420beAD13e6bBB79b7E6F034298);
+        priceFeeds[2] = address(0xb113F5A928BCfF189C998ab20d753a47F9dE5A61);
 
         AMM amm = _deployAMM({tokens: underlyings, priceFeeds: priceFeeds});
         console.log("Mocked AMM deployed at", address(amm));
@@ -35,15 +39,17 @@ contract DeploymentScript is Script {
         usdc.setCanMint({minter: address(amm), value: true});
         weth.setCanMint({minter: address(amm), value: true});
         wbtc.setCanMint({minter: address(amm), value: true});
+        link.setCanMint({minter: address(amm), value: true});
 
         // 4. Deploy the strategy factory.
         StrategyFactory factory = _deployStrategyFactory({usdc: address(usdc), amm: amm});
         console.log("Strategy factory deployed at", address(factory));
 
         // 5. Deploy a strategy.
-        uint16[] memory weights = new uint16[](2);
+        uint16[] memory weights = new uint16[](3);
         weights[0] = 50_00;
-        weights[1] = 50_00;
+        weights[1] = 25_00;
+        weights[2] = 25_00;
         address[] memory investors;
         address strategy = factory.deployNewStrategy({
             tokens: underlyings,
@@ -51,8 +57,8 @@ contract DeploymentScript is Script {
             rebalanceInterval: 1 days,
             isOpen: true,
             investors: investors,
-            name: "50/50 WETH WBTC",
-            symbol: "50/50 WETH WBTC"
+            name: "50/25/25 WETH WBTC LINK",
+            symbol: "50/25/25 WETH WBTC LINKC"
         });
 
         console.log("Strategy deployed at", address(strategy));
